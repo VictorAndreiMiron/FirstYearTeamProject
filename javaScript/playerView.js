f5bc8d6aced9cc8828bee39c4693d3ae017d81cf
VictorAndreiMiron <andrei2000.miron@gmail.com>
2019-03-07 00:25:37 +0000

    host view and player view integrated. (still need timer for host view though)

--- javaScript/playerView.js
+++ javaScript/playerView.js
@@ -1,17 +1,16 @@
-  var socket = io.connect('http://localhost:4000/');
+var socket = io.connect('http://localhost:4000/');
 
-  function vote(option)
-  {
-    socket.emit("vote",{option : option});
-     document.getElementById('plot').innerHTML = 'test1';
-     document.getElementById('button1').disabled = true;
-     document.getElementById('button2').disabled = true;
-     document.getElementById('button3').disabled = true;
+function vote(option)
+{
+   socket.emit("vote",{option : option});
+   document.getElementById('button0').disabled = true;
+   document.getElementById('button1').disabled = true;
+   document.getElementById('button2').disabled = true;
 
-  }
+}
 
-  socket.on("enableVote",function(){
-    document.getElementById('button1').disabled = false;
-    document.getElementById('button2').disabled = false;
-    document.getElementById('button3').disabled = false;
-  });
+socket.on("enableVote",function(){
+  document.getElementById('button0').disabled = false;
+  document.getElementById('button1').disabled = false;
+  document.getElementById('button2').disabled = false;
+});
